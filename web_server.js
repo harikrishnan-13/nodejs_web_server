c193ca1986b020c7d2bd80649739b70464ead427
harikrishnan-13 <135119257+harikrishnan-13@users.noreply.github.com>
2024-01-09 08:55:42 +0530

    upto routing

--- web_server.js
+++ web_server.js
@@ -25,43 +25,11 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'./public')));
+app.use('/',express.static(path.join(__dirname,'./public')));
+app.use('/subdir',express.static(path.join(__dirname,'./public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301,'new-page.html');
-});
-
-app.get('/hello(.html)?', (req, res , next) => {
-    console.log('logged successfully');
-    next()
-},(req, res) => {
-    res.send("Hello harikrishnan!");
-});
-
-const one= (req, res,next) => {
-    console.log('one');
-    next();
-};
-
-const two= (req, res,next) => {
-    console.log('two');
-    next();
-};
-
-const three= (req, res) => {
-    console.log('three');
-    res.send('finished');
-};
-
-app.get('/chain(.html)?',[one,two,three]);
+app.use('/',require('./routes/root'));
+app.use('/subdir',require('./routes/subdir'));
 
 // app.get('/*', (req, res) => {
 //     res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
@@ -83,5 +51,3 @@ app.all('/*', (req, res) => {
 
 app.use(errorHandler);
 app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
-
-//jadhfuksdhfsf
